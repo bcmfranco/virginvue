04ae0de198f01d8509fbe96ae9960d7804fdf652
bcmfranco <bruno@codev.com.ar>
2024-02-12 22:44:38 -0300

    add all

---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Virgin vue</h1>
+</template>
+
+<script>
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+*{
+  font-family: 'Roboto', sans-serif;
+}
+
+h1{
+  text-align: center;
+}
+
+
+</style>
